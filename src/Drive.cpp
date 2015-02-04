0e5632f8f5f58e6fcbeddb98e75c537280c15347
Philip Thomas <tucono@gmail.com>
2015-02-03 20:19:18 -0800

    Continuing to work on controlled acceleration. May need to use accelerometer instead.

--- src/Drive.cpp
+++ src/Drive.cpp
@@ -13,17 +13,14 @@ class Drive : public Spyder::Subsystem
 private:
 	RobotDrive *m_robotDrive;
 	Joystick *driveStick;
-	//Spyder::ConfigVar<bool> test
 	Accelerometer *accel;
-	double maxAccel;
-	double accelDiffX;
-	double accelDiffY;
 	float driveX;
 	float curveX;
 	float driveY;
 	float curveY;
 	float twist;
 	float curveT;
+	float rampVal;
 
 public:
 	Drive() : Spyder::Subsystem("Drive")
@@ -37,9 +34,10 @@ public:
 
 	virtual void Init(Spyder::RunModes runmode)
 	{
-		Spyder::ConfigVar<double> mAccel("maxAccelerationVal", 2);
-		maxAccel = mAccel.GetVal();
-		accel = new BuiltInAccelerometer();
+		Spyder::ConfigVar<double> mAccel("maxAccelerationVal", 0.5);
+		Spyder::ConfigVar<uint32_t> leftFrontMotor("leftFrontDriveMotor", 2);
+		//leftTopMotor = Spyder::GetVictor(leftFrontMotor.GetVal());
+		//accel = new BuiltInAccelerometer();
 		m_robotDrive = new RobotDrive(2,3,1,0);
 		m_robotDrive->SetExpiration(0.1);
 		m_robotDrive->SetInvertedMotor(RobotDrive::kFrontLeftMotor, true);	// invert the left side motors
@@ -59,26 +57,30 @@ public:
 
 	virtual void Periodic(Spyder::RunModes runmode)
 	{
+		Spyder::TwoIntConfig rightJoystick("rightJoyBind", 0, 1);
+		driveStick = Spyder::GetJoystick(rightJoystick.GetVar(1));
 		driveX = 0.0f;
 		curveX = 0.0f;
 		driveY = 0.0f;
 		curveY = 0.0f;
 		twist = 0.0f;
 		curveT = 0.0f;
-		accelDiffX = 0.0f;
-		accelDiffY = 0.0f;
-		double xAccel = accel->GetX();
-		double yAccel = accel->GetY();
-		Spyder::TwoIntConfig rightJoystick("rightJoyBind", 0, 1);
-		driveStick = Spyder::GetJoystick(rightJoystick.GetVar(1));
+		rampVal = 0.01;
+		//float rampY = 0.0f;
+		//double xAccel = accel->GetX();
+		//double yAccel = accel->GetY();
 		driveStick->SetAxisChannel(Joystick::kTwistAxis, 2);
+
 		switch(runmode)
 		{
 		case Spyder::M_AUTO:
 			break;
+
 		case Spyder::M_DISABLED:
 			break;
+
 		case Spyder::M_TELEOP:
+			//rampY = leftTopMotor->Get();
 			driveX = driveStick->GetRawAxis(rightJoystick.GetVar(1));
 			driveY = driveStick->GetRawAxis(rightJoystick.GetVar(2));
 			twist = driveStick->GetTwist();
@@ -86,40 +88,13 @@ public:
 			driveY = fabs(driveY) > Spyder::GetDeadzone() ? driveY : 0;
 			twist = fabs(twist) > Spyder::GetDeadzone() ? twist : 0;
 
-			accelDiffX = xAccel-(3*driveX*driveX);
-			accelDiffY = yAccel-(3*driveY*driveY);
-
-			/*if(accelDiffX >= maxAccel)//Acceleration throttle on the x-axis
+			/*curveY = (rampY + (driveY * driveY * driveY))/10;
+			if(curveY < 0.15 && curveY > -0.15)
 			{
-				driveX = driveX + maxAccel;//if lower, increase speed by max
-				curveX = driveX;
-			}
-			else if(accelDiffX <= -maxAccel )
-			{
-				driveX = driveX - maxAccel;//if higher, decrease speed by max
-				curveX = driveX;
-			}
-			else
-			{
-				curveX = driveX * driveX * driveX;//normal curve drive
-			}
-
-			if(accelDiffY >= maxAccel)//Acceleration throttle on the y-axis
-			{
-				driveY = driveY + maxAccel;//if lower, increase speed by max
-				curveY = driveY;
-			}
-			else if(accelDiffY <= -maxAccel)
-			{
-				driveY = driveY - maxAccel;//if higher, decrease speed by max
-				curveY = driveY;
-			}
-			else
-			{
-				curveY = driveY * driveY * driveY;//normal curve drive
+				curveY = 0;
 			}*/
-			curveX = driveX * driveX * driveX;
 			curveY = driveY * driveY * driveY;
+			curveX = driveX * driveX * driveX;
 			curveT = twist * twist * twist;
 
 			m_robotDrive->MecanumDrive_Cartesian(curveX, curveY, curveT);
