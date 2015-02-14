25eb83f148510bffab414351fdb274860353f664
Philip Thomas <tucono@gmail.com>
2015-02-14 10:57:57 -0800

    Removed unneeded drive.cpp and cleaned up claw.cpp. Should work when needed.

--- src/Claw.cpp
+++ src/Claw.cpp
@@ -10,6 +10,8 @@ class Claw : public Spyder::Subsystem
 private:
 	bool openClawButton;
 	bool closeClawButton;
+	Solenoid *clawSolExtend;
+	Solenoid *clawSolRetract;
 
 public:
 	Claw() : Spyder::Subsystem("Claw")
@@ -22,6 +24,11 @@ public:
 
 	virtual void Init(Spyder::RunModes runmode)
 	{
+		Spyder::ConfigVar<uint32_t> extendSol("clawExtendSolPort", 0);//Initialize the solenoids!
+		Spyder::ConfigVar<uint32_t> retractSol("clawRetractSolPort", 1);
+		clawSolExtend = new Solenoid (extendSol.GetVal());
+		clawSolRetract = new Solenoid (retractSol.GetVal());
+
 		switch(runmode)
 		{
 		case Spyder::M_AUTO:
@@ -34,26 +41,26 @@ public:
 	}
 	virtual void Periodic(Spyder::RunModes runmode)
 	{
-		Spyder::TwoIntConfig openClaw ("openClawButtonVal", 2, 7);
+		Spyder::TwoIntConfig openClaw ("openClawButtonVal", 2, 7);//Initialize the claw ports
 		Spyder::TwoIntConfig closeClaw ("closeClawButtonVal", 2, 8);
-		Spyder::ConfigVar<uint32_t> extendSol("clawExtendSolPort", 0);
-		Spyder::ConfigVar<uint32_t> retractSol("clawRetractSolPort", 1);
-		openClawButton = Spyder::GetJoystick(openClaw.GetVar(1))->GetRawButton(openClaw.GetVar(2));
-		closeClawButton = Spyder::GetJoystick(closeClaw.GetVar(1))->GetRawButton(closeClaw.GetVar(2));
+
+
 		switch(runmode)
 		{
 		case Spyder::M_AUTO:
 			break;
 		case Spyder::M_TELEOP:
-			if(openClawButton)
+			openClawButton = Spyder::GetJoystick(openClaw.GetVar(1))->GetRawButton(openClaw.GetVar(2));//Get Button Vals
+			closeClawButton = Spyder::GetJoystick(closeClaw.GetVar(1))->GetRawButton(closeClaw.GetVar(2));
+			if(openClawButton)//Open Claw
 			{
-				Spyder::GetSolenoid(extendSol.GetVal())->Set(false);
-				Spyder::GetSolenoid(retractSol.GetVal())->Set(true);
+				clawSolExtend->Set(false);//Set false solenoids before setting true
+				clawSolRetract->Set(true);
 			}
-			if(closeClawButton)
+			if(closeClawButton)//Close Claw
 			{
-				Spyder::GetSolenoid(extendSol.GetVal())->Set(true);
-				Spyder::GetSolenoid(retractSol.GetVal())->Set(false);
+				clawSolRetract->Set(false);
+				clawSolExtend->Set(true);
 			}
 			break;
 		default:
